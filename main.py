5f0690df3a94bd8437871930ec75c770ea3e847b
ReshiramRocks12 <soham.walimbe29@gmail.com>
2025-08-01 16:13:11 +0800

    Allow for RTC connections

--- main.py
+++ main.py
@@ -1,6 +1,52 @@
+import json
 import ssl
 
+from aiortc import RTCPeerConnection, RTCSessionDescription
 from aiohttp import web, web_request
+
+connection: RTCPeerConnection = None
+
+async def rtc_start_handle(request: web_request.Request) -> web.Response:
+	global connection
+
+	try:
+		data = await request.json()
+
+		if connection:
+			raise RuntimeError('Unable to create a new connection: Connection not closed.')
+
+		connection = RTCPeerConnection()
+
+		# Get the session description
+		sdp = data['sdp']
+		sdp_type = data['type']
+		description = RTCSessionDescription(sdp, sdp_type)
+		await connection.setRemoteDescription(description)
+
+		# Create an answer with ICE candidates to respond to the offer
+		answer = await connection.createAnswer()
+		await connection.setLocalDescription(answer)
+
+		return web.Response(
+			content_type='application/json',
+			text=json.dumps({ 'sdp': answer.sdp, 'type': answer.type })
+		)
+	except Exception as e:
+		return web.Response(text=str(e), status=400)
+
+async def rtc_stop_handle(request: web_request.Request) -> web.Response:
+	global connection
+
+	try:
+		if not connection:
+			raise RuntimeError('Unable to close the connection: Connection not found.')
+
+		await connection.close()
+		connection = None
+
+		return web.Response()
+	except Exception as e:
+		return web.Response(text=str(e), status=400)
 
 async def index_handle(request: web_request.Request) -> web.FileResponse:
 	return web.FileResponse(r'.\index.html')
@@ -15,10 +61,14 @@ def init_routes(app: web.Application) -> None:
 	app.router.add_static('/static/', path=r'.\static', name='static') # Static resources
 	app.router.add_get('/', index_handle) # Leads to index.html
 
+	app.router.add_post('/rtcStart', rtc_start_handle) # Route to signal start of stream
+	app.router.add_post('/rtcStop', rtc_stop_handle) # Route to signal end of stream
+
 def main() -> int:
 	app = web.Application()
 	init_routes(app)
 
+	# SSL to enable HTTPS
 	cert_file = r'.\secrets\cert.pem'
 	key_file = r'.\secrets\key.pem'
 	ssl_context = create_ssl_context(cert_file, key_file)
--- static/sender.js
+++ static/sender.js
@@ -1,7 +1,70 @@
 
+var peerConnection;
+
 function onStreamToggle()
 {
-	
+	const streamToggleButton = document.getElementById('streamToggle');
+
+	if (peerConnection)
+	{
+		closeConnection().catch(console.error);
+		streamToggleButton.innerHTML = 'Start Streaming';
+	}
+	else
+	{
+		startConnection();
+		streamToggleButton.innerHTML = 'Stop Streaming';
+	}
+}
+
+function postJSON(site, body)
+{
+	return fetch(site,
+		{
+			method: 'POST',
+			headers:
+			{
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(body)
+		});
+}
+
+async function onIceGatheringStateChange()
+{
+	if (peerConnection.iceGatheringState == 'complete')
+	{
+		const response = await postJSON('/rtcStart', peerConnection.localDescription.toJSON());
+		response.json().then(async data => await peerConnection.setRemoteDescription(data));
+		const streamToggleButton = document.getElementById('streamToggle');
+		streamToggleButton.disabled = false;
+	}
+}
+
+function createPeerConnection()
+{
+	peerConnection = new RTCPeerConnection();
+	const cameraVideoElement = document.getElementById('camera');
+	cameraVideoElement.srcObject.getTracks().forEach(track => peerConnection.addTrack(track));
+}
+
+function startConnection()
+{
+	const streamToggleButton = document.getElementById('streamToggle');
+	streamToggleButton.disabled = true;
+	createPeerConnection();
+	peerConnection.createOffer().then(async offer => await peerConnection.setLocalDescription(offer));
+	peerConnection.onicegatheringstatechange = onIceGatheringStateChange;
+}
+
+async function closeConnection()
+{
+	const streamToggleButton = document.getElementById('streamToggle');
+	streamToggleButton.disabled = true;
+	await postJSON('/rtcStop', {});
+	await peerConnection.close();
+	peerConnection = null;
+	streamToggleButton.disabled = false;
 }
 
 document.addEventListener('DOMContentLoaded', () =>
@@ -17,10 +80,8 @@ document.addEventListener('DOMContentLoaded', () =>
 	streamToggleButton.onclick = onStreamToggle;
 });
 
-window.addEventListener('pagehide', event =>
+window.addEventListener('pagehide', async event =>
 {
 	if (!event.persisted)
-	{
-		// TODO: Exit logic
-	}
+		await closeConnection();
 });
