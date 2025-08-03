60d1cd1f76503979a0dc65d132c5b4fb94cc9a1a
ReshiramRocks12 <soham.walimbe29@gmail.com>
2025-08-03 14:03:16 +0800

    Allow arguments to be provided. Close RTC connection on shutdown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import argparse
 import asyncio
 import json
 import ssl
+import sys
 
 import aiortc
 import cv2
@@ -12,7 +14,7 @@ from aiortc import rtcrtpreceiver
 connection: aiortc.RTCPeerConnection = None
 settings = None
 
-def createConnection() -> None:
+def create_connection() -> None:
 	global connection
 	
 	if connection:
@@ -20,6 +22,15 @@ def createConnection() -> None:
 
 	connection = aiortc.RTCPeerConnection()
 	connection.add_listener('track', on_track_recieved)
+
+async def close_connection() -> None:
+	global connection
+	
+	if not connection:
+		raise RuntimeError('Unable to close the connection: Connection not found.')
+
+	await connection.close()
+	connection = None
 	
 async def index_handle(request: web_request.Request) -> web.FileResponse:
 	return web.FileResponse(r'.\index.html')
@@ -28,7 +39,7 @@ async def rtc_start_handle(request: web_request.Request) -> web.Response:
 	global connection
 
 	try:
-		createConnection()
+		create_connection()
 
 		data = await request.json()
 
@@ -53,11 +64,7 @@ async def rtc_stop_handle(request: web_request.Request) -> web.Response:
 	global connection
 
 	try:
-		if not connection:
-			raise RuntimeError('Unable to close the connection: Connection not found.')
-
-		await connection.close()
-		connection = None
+		await close_connection()
 		cv2.destroyAllWindows()
 
 		return web.Response()
@@ -87,7 +94,7 @@ def init_routes(app: web.Application) -> None:
 
 	app.router.add_post('/rtcStart', rtc_start_handle) # Route to signal start of stream
 	app.router.add_post('/rtcStop', rtc_stop_handle) # Route to signal end of stream
-	app.router.add_post('/settings', settings_handle) # Route to signal end of stream
+	app.router.add_post('/settings', settings_handle) # Route to send camera settings
 
 async def handle_video(track: rtcrtpreceiver.RemoteStreamTrack) -> None:
 	global settings
@@ -107,22 +114,46 @@ async def handle_video(track: rtcrtpreceiver.RemoteStreamTrack) -> None:
 		if connection: # If this is not caused by the connection closing
 			print(str(e))
 
+async def on_webapp_shutdown(app: web.Application):
+	if connection:
+		await close_connection()
+
 def on_track_recieved(track: rtcrtpreceiver.RemoteStreamTrack) -> None:
 	if track.kind == 'video':
 		asyncio.create_task(handle_video(track))
 
+def parse_arguments() -> argparse.Namespace:
+	parser = argparse.ArgumentParser('Runs a web server to recieve a camera stream.')
+	parser.add_argument('--host', type=str, default='0.0.0.0', help='Network interface to bind the server to')
+	parser.add_argument('--port', type=int, default=8080, help='Port to bind the server to')
+	parser.add_argument('--certfile', type=str, default=r'.\cert.pem', help='A path to an SSL certificate file')
+	parser.add_argument('--keyfile', type=str, default=r'.\key.pem', help='A path to an SSL private key file')
+	parser.add_argument('--password', type=str, default=None, help='The password of the SSL private key')
+	
+	return parser.parse_args()
+
 def main() -> int:
+	args = parse_arguments()
+
 	app = web.Application()
 	init_routes(app)
+	app.on_shutdown.append(on_webapp_shutdown)
 
-	# SSL to enable HTTPS
-	cert_file = r'.\secrets\cert.pem'
-	key_file = r'.\secrets\key.pem'
-	ssl_context = create_ssl_context(cert_file, key_file)
+	try:
+		# SSL to enable HTTPS
+		cert_file = args.certfile
+		key_file = args.keyfile
+		password = args.password
+		ssl_context = create_ssl_context(cert_file, key_file, password)
+	except Exception as e:
+		print(f'An error occurred creating an SSL context: {str(e)}')
+		return 1
 
-	# Run HTTPS application exposed to all devices on the local network
-	web.run_app(app, host='0.0.0.0', port=8080, ssl_context=ssl_context)
+	# Run HTTPS application on the provided network interface and port
+	host = args.host
+	port = args.port
+	web.run_app(app, host=host, port=port, ssl_context=ssl_context)
 	return 0
 
 if __name__ == '__main__':
-	exit(main())
+	sys.exit(main())
